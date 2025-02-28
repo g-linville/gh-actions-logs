1ce417de06bb7fcb5807f144b7476b4f55933e02
Grant Linville <grant@acorn.io>
2025-02-27 23:04:19 -0500

    more files
    Signed-off-by: Grant Linville <grant@acorn.io>

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+
+def setup_auth_headers():
+    token = os.environ.get('TOKEN')
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'User-Agent': 'Python',
+        'Authorization': f"token {token}"
+    }
+    
+    return headers
+
+def main():
+    job_id = os.environ.get('job_id')
+    owner = os.environ.get('owner')
+    repo = os.environ.get('repo')
+
+    # Set up authentication headers
+    headers = setup_auth_headers()
+    
+    url = f"https://api.github.com/repos/{owner}/{repo}/actions/jobs/{job_id}/logs"
+    response = requests.get(url, headers=headers)
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
